d7ad3f56978889d5323c5f35c03ea2893f21d387
Topher <cjones@sycamoreleaf.com>
2014-12-03 09:54:25 -0600

    Version 2.1

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
 $(document).ready(function() {
-   $('.buttonanimator').mouseenter(function() {
-       	   $(this).animate({
-           marginTop: '+=10px'
-       });
-   });
-   $('.buttonanimator').mouseleave(function() {
-       $(this).animate({
-           marginTop: '-=10px'
-       }); 
-   });
-   $('.profile-pic').mouseenter(function() {
-       	   $(this).animate({
-           marginLeft: '+=10px'
-       });
-   });
-   $('.profile-pic').mouseleave(function() {
-       $(this).animate({
-           marginLeft: '-=10px'
-       }); 
-   });
-	$("#terd").imageLightbox({});
+   	$("#terd").imageLightbox({});
 });
