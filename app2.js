bfef073edf627c7848ed07b11a2a58a7dd22a6a5
Topher <cjones.wingsofgold@gmail.com>
2014-12-21 01:44:26 -0600

    3.9.1

--- app2.js
+++ app2.js
@@ -1,19 +1,17 @@
 $(document).ready(function(){
+         // this is to start the blog buttons off as full opacity
          $('.hideMe1').fadeTo('slow', 1.0);
          $('.hideMe2').fadeTo('slow', 1.0);
          $('.hideMe3').fadeTo('slow', 1.0);
 
 
-
+// this fades the blog buttons with mouse overs and returns them to full opacity when mouse exits
   $('.hideMe1').mouseenter(function() {
     $(this).fadeTo('fast', 0.45);
   });
   $('.hideMe1').mouseleave(function() {
     $(this).fadeTo('slow', 1.0);
   });
-  
-  
-  
   
   $('.hideMe2').mouseenter(function() {
     $(this).fadeTo('fast', 0.45);
@@ -22,9 +20,6 @@ $(document).ready(function(){
     $(this).fadeTo('slow', 1.0);
   });
   
-  
-  
-  
   $('.hideMe3').mouseenter(function() {
     $(this).fadeTo('fast', 0.45);
   });
@@ -32,6 +27,11 @@ $(document).ready(function(){
     $(this).fadeTo('slow', 1.0);
   });
   
+  
+  //utilizing the .scrollto function to move to parts of the page when clicking topNav links
+  $('.contactLink').click(function(){
+  $('haleyContactHome').scrollTo();
+});
   
   
 });
