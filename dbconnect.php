75b097f822c9f49f80d82dadb9b0e895118052be
TheBrockEllis <brock@sharproot.com>
2014-12-30 15:34:57 -0700

    Removed file from being tracked

--- dbconnect.php
+++ dbconnect.php
@@ -2,5 +2,5 @@
 require_once("passwords.php");
 
 $dblink = mysql_connect($db_host, $db_user, $db_password);
-$sc = mysql_select_db("haleyblog", $dblink);
+$sc = mysql_select_db("sharproo_haleyblog", $dblink);
 ?>
