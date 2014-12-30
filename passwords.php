1c8b46b731db7e6f124a4a7f90f4d01fff310ab7
Topher <cjones@sycamoreleaf.com>
2014-12-30 16:03:17 -0600

    4.0.2

---
+++ passwords.php
@@ -0,0 +1,10 @@
+<?php
+$user_name = "iAmAHUFFL3PUFF";
+$user_password = "ST1LL?alway5";
+$db_host = "localhost";
+$db_user = "root";
+$db_password = "";
+
+$cookie_name = "loggedin";
+$cookie_value = "31dec2013";
+?>
