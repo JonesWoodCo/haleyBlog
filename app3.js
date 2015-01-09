cc0a6691dd17821fb2b7c16010d69f3a4081edbf
Topher <cjones@sycamoreleaf.com>
2015-01-08 20:09:31 -0600

    5.1.8.3

--- app3.js
+++ app3.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
          $('.login').hide();
+         $('.messageMe').hide();
 
 
   $('.loginAppear').click(function() {
     $('.login').toggle();
+  });
+  $('.messageDrop').click(function() {
+    $('.messageMe').toggle();
   });
 });
