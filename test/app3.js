375938aea7624173719e360a3ebe3a8c01d88331
Topher <cjones@sycamoreleaf.com>
2015-01-14 17:05:34 -0600

    messing around with the emailing

---
+++ test/app3.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+         $('.login').hide();
+         $('.messageMe').hide();
+
+
+  $('.loginAppear').click(function() {
+    $('.login').toggle();
+  });
+  $('.messageDrop').click(function() {
+    //$('.messageMe').slideDown( "slow", function() {
+    // Animation complete.
+  //});
+  $('.messageMe').toggle();
+  
+  });
+});
