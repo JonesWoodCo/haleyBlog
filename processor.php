3de0df848f329db1ee08ddc2fddca45ee91d8d1a
Topher <cjones@sycamoreleaf.com>
2015-01-16 14:37:36 -0600

    Packers win superbowl

--- processor.php
+++ processor.php
@@ -4,6 +4,6 @@ $sender=$_POST['email'];
 $subject=$_POST['subject'];
 $message=$_POST['message'];
 
-$email=$name . "\n" . $sender . "\n" . $message;
+$email="Name: " . $name . "\n" . "Email: " . $sender . "\n" . "Subject: " . $subject . "\n" . "Message: " . $message;
 mail("cjones@sycamoreleaf.com", $subject, $email);
 ?>
