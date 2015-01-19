863b0aa26fa8faaf6c711ef9c48104d74a32b72f
Topher <cjones@sycamoreleaf.com>
2015-01-19 17:15:35 -0600

    new blog page hoisting.

--- blog.php
+++ blog.php
@@ -9,7 +9,7 @@ $postid=$_GET['ID'];
 	<head>
 		<meta charset="UTF-8">
 		<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-		<link href="css/main.css" rel="stylesheet" type="text/css"/>
+		<link href="css/testmain.css" rel="stylesheet" type="text/css"/>
 		<script type='text/javascript' src='js/jquery.js'></script>
 		<script type='text/javascript' src='js/jquery-ui.js'></script>
 		<script type='text/javascript' src='js/jquery-scrollto.js'></script>
@@ -50,69 +50,53 @@ $postid=$_GET['ID'];
 			</div>	
 		</div>
 			
-		<?php
-	$sql = "select * from blogs where ID=$postid";
-	    echo("SQL: $sql");
-	 $rs = mysql_query($sql);
-	  if($rs) $rsc = mysql_num_rows($rs);
-	  for($i=0; $i < $rsc; $i++){
-		$title = mysql_result($rs, $i, "Title");
-		$created = mysql_result($rs, $i, "Created");
-		$photo = mysql_result($rs, $i, "Photo");
-		$hover = mysql_result($rs, $i, "Hover");
-		$publisher = mysql_result($rs, $i, "Publisher");
-		$content = mysql_result($rs, $i, "Content");
-		$articlephoto = mysql_result($rs, $i, "Articlephoto");
-	    echo "Column1 is $photo and column 2 is $hover";
-	  }
-	?>  
-		<div class="container">	
-			<div class="content">
-				<div class="mainArticleSpace ">
-					<h1><?php echo $title?></h1> 
+                        
+                         <div class="haleyBlogBlog">
+      
+                            <?php
+                                $sql = "select * from blogs limit 5";
+                                    //echo("SQL: $sql");
+                                 $rs = mysql_query($sql);
+                                    if($rs) $rsc = mysql_num_rows($rs);
+                                        for($i=0; $i < $rsc; $i++){
+                                            $photo = mysql_result($rs, $i, "Photo");
+                                            $postid = mysql_result($rs, $i, "ID");
+                                            $hover = mysql_result($rs, $i, "Hover");
+                                            //echo "Column1 is $photo and column 2 is $hover";
+                                          echo "<div class='blogTile'>
+                                                        <div class='blogbackground' style='height: 150px;. width: 150px; background-image: url( $photo);'>
+                                                            <a href='blog.php?ID=$postid'><img class='hideMe1' src=' $hover ' style='background-image: url( $photo); height: 150px; width: 150px;'></a>
+                                                        </div>
+                                                    </div>"	  ;
+                                        }
+                            ?>
+                            
+                        </div>
+                        <?php
+                            $sql = "select * from blogs where ID=$postid";
+                                //echo("SQL: $sql");
+                             $rs = mysql_query($sql);
+                              if($rs) $rsc = mysql_num_rows($rs);
+                              for($i=0; $i < $rsc; $i++){
+                                    $title = mysql_result($rs, $i, "Title");
+                                    $created = mysql_result($rs, $i, "Created");
+                                    $photo = mysql_result($rs, $i, "Photo");
+                                    $hover = mysql_result($rs, $i, "Hover");
+                                    $publisher = mysql_result($rs, $i, "Publisher");
+                                    $content = mysql_result($rs, $i, "Content");
+                                    $articlephoto = mysql_result($rs, $i, "Articlephoto");
+                                //echo "Column1 is $photo and column 2 is $hover";
+                              }
+                            ?>  
+                            <div class="haleyContentBlog">
+                              <h1> <b style="font-size: 1.4em"><?php echo $title?></b></h1>
 					<h5><i><?php echo $created?></i></h5>
 					<h6>By Haley Halverson. Published by <?php echo $publisher ?></h6>
-					<img style="width: 100%; " src="<?php echo $articlephoto?>">
-					<p><?php echo $content ?></p>			
-				</div>
-				
-	<?php
-		$sql = "select * from blogs limit 10";
-		//echo("SQL: $sql");
-		$rs = mysql_query($sql);
-		if($rs) $rsc = mysql_num_rows($rs);
-		for($i=0; $i < $rsc; $i++){
-			$title = mysql_result($rs, $i, "Title");
-			$created = mysql_result($rs, $i, "Created");
-			$photo = mysql_result($rs, $i, "Photo");
-			$hover = mysql_result($rs, $i, "Hover");
-			$content = mysql_result($rs, $i, "Content");
-			$postid = mysql_result($rs, $i, "ID");
-			
-			
-		//echo "Column1 is $photo and column 2 is $hover";
-		
-		
-		
-	
-		
-		
-			echo	"<div class='blogSideMenu '>
-						<div class='articles'>						
-							<div class='sideblogtile' style='width: 150px; height: 150px;  background-image: url( $photo);'>
-								<div class='article-1'>	
-									<a href=' blog.php?ID=$postid'>
-									<img class='hideMe1' src=' $hover ' style='width: 150px; height: 150px;'>
-									</a>
-								</div>
-							</div>							
-						</div>												
-					</div>";
-		}
-	?>
-				
-				</div>
-			</div>		
+					<img style="width: 100%; max-width: 600px; " src="<?php //echo $articlephoto?>">
+					<p><?php echo $content ?></p>
+                            </div>   
+                        
+                        
 			<div class="socialFooterLinks"><ul class="footerSocial" >
 								<li><a href="https://twitter.com/_hhalverson" class="footerTwitter" ><img src="img/twitterFooter.png"></a></li>
 								<li>|</li>
